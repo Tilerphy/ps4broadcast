d1ca5127e8cea37e0f7e8512424b6a3af05de074
Zonas Sun <346410840@163.com>
2017-10-19 09:37:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ sed -i "s/#user  nobody;/rtmp { include \/usr\/local\/nginx\/conf\/rtmp.conf.d\/
 echo "Configure iptables................."
 #remove old
 #sed -i "s/iptables -N ps4broadcast -t nat//g" /etc/rc.local
-sed -i "s/ifconfig eth0:2 192.168.200.1 netmask 255.255.255.0//g" /etc/rc.local
+sed -i "s/ifconfig $1:2 192.168.200.1 netmask 255.255.255.0//g" /etc/rc.local
 sed -i "s/sysctl -w net.ipv4.ip_forward=1//g" /etc/rc.local
 sed -i "s/sysctl -p//g" /etc/rc.local
 sed -i "s/iptables -t nat -A PREROUTING --ipv4 -s 192.168.200.1 -j RETURN//g" /etc/rc.local
@@ -39,7 +39,7 @@ sed -i "s/iptables -t nat -A PREROUTING -p tcp -s 192.168.200.0\/24 --dport 6667
 sed -i "s/iptables -t nat -A POSTROUTING --ipv4 -j MASQUERADE//g" /etc/rc.local
 #add back
 #sed -i "s/exit 0/iptables -N ps4broadcast -t nat\nexit 0/g" /etc/rc.local
-sed -i "s/exit 0/ifconfig eth0:2 192.168.200.1 netmask 255.255.255.0\nexit 0/g" /etc/rc.local
+sed -i "s/exit 0/ifconfig $1:2 192.168.200.1 netmask 255.255.255.0\nexit 0/g" /etc/rc.local
 sed -i "s/exit 0/sysctl -w net.ipv4.ip_forward=1\nexit 0/g" /etc/rc.local
 sed -i "s/exit 0/sysctl -p\nexit 0/g" /etc/rc.local
 sed -i "s/exit 0/iptables -t nat -A PREROUTING --ipv4 -s 192.168.200.1 -j RETURN\nexit 0/g" /etc/rc.local
@@ -49,7 +49,7 @@ sed -i "s/exit 0/iptables -t nat -A PREROUTING -p tcp -s 192.168.200.0\/24 --dpo
 sed -i "s/exit 0/iptables -t nat -A POSTROUTING --ipv4 -j MASQUERADE\nexit 0/g" /etc/rc.local
 #real do
 #iptables -N ps4broadcast -t nat
-ifconfig eth0:2 192.168.200.1 netmask 255.255.255.0
+ifconfig $1:2 192.168.200.1 netmask 255.255.255.0
 sysctl -w net.ipv4.ip_forward=1
 sysctl -p
 iptables -t nat -A PREROUTING --ipv4 -s 192.168.200.1 -j RETURN
