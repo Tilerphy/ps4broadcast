0f81c2e28eab9fa85443967b04ce8945787dd755
Zonas Sun <346410840@163.com>
2023-08-27 07:27:44 +0800

    Update room_bilibili.js

--- modules/room_bilibili.js
+++ modules/room_bilibili.js
@@ -4,36 +4,28 @@ const { deflate, unzip,inflate } = require('zlib');
 
 var buvidString  = "";
 var buvid ="";
-//xhttps.get('https://api.bilibili.com/x/frontend/finger/spi', (response) => {
-//      response.on("data", (d)=>{
-//              buvidString += d;
-//      });
-//
-//      response.on('end', () => {
-//              var buvidObj = JSON.parse(buvidString);
-//              buvid = buvidObj["data"]["b_3"];
-//              console.log("buvid is "+ buvid);
-//      });
-//
-//});
 
-var init = function(rid, io, lp){
-                xhttps.get('https://api.bilibili.com/x/frontend/finger/spi', (response) => {
-                        response.on("data", (d)=>{
-                                buvidString += d;
-                        });
-
-                        response.on('end', () => {
-                                var buvidObj = JSON.parse(buvidString);
-                                buvid = buvidObj["data"]["b_3"];
-                                console.log("buvid is "+ buvid);
-                                initx(rid, io, lp);
-                        });
-
+function refreshToken(){
+        buvidString = "";
+        xhttps.get('https://api.bilibili.com/x/frontend/finger/spi', (response) => {
+                response.on("data", (d)=>{
+                        buvidString += d;
                 });
+
+                response.on('end', () => {
+                        var buvidObj = JSON.parse(buvidString);
+                        buvid = buvidObj["data"]["b_3"];
+                        console.log("buvid is "+ buvid);
+                });
+
+        });
+
 }
 
-var initx = function (rid, io, lp){
+setInterval(refreshToken, 10000);
+
+
+var init = function (rid, io, lp){
         this.uid = 123456789012345 + parseInt(1000000000000000*Math.random());
         this.rid = rid;
         this.lp = lp;
